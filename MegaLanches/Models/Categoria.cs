22455e944223dea606d4616b7b0908d2aec22aa8
rafaelg <romilson.2r@gmail.com>
2021-06-01 19:02:54 -0300

    Create Models

---
+++ MegaLanches/Models/Categoria.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace MegaLanches.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+        public string CategoriaNome { get; set; }
+        public string Descricao { get; set; }
+        public List<Lanche> Lanches { get; set; }
+    }
+}
