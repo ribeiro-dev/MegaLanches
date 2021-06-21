1587cd827e09cb2340317e14a3cb72765cbabb61
rafaelg <romilson.2r@gmail.com>
2021-06-21 20:01:59 -0300

    Implementing ViewComponent

---
+++ MegaLanches/Components/CarrinhoCompraResumo.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using MegaLanches.Models;
+using MegaLanches.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MegaLanches.Components
+{
+    public class CarrinhoCompraResumo : ViewComponent
+    {
+        private readonly CarrinhoCompra _carrinhoCompra;
+
+        public CarrinhoCompraResumo(CarrinhoCompra carrinhoCompra)
+        {
+            _carrinhoCompra = carrinhoCompra;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            //var itens = _carrinhoCompra.GetCarrinhoCompraItens();
+            var itens = new List<CarrinhoCompraItem> { new CarrinhoCompraItem(), new CarrinhoCompraItem() };
+            _carrinhoCompra.CarrinhoCompraItens = itens;
+
+            var carrinhoCompraVM = new CarrinhoCompraViewModel
+            {
+                CarrinhoCompra = _carrinhoCompra,
+                CarrinhoCompraTotal = _carrinhoCompra.GetCarrinhoCompraTotal()
+            };
+
+            return View(carrinhoCompraVM);
+        }
+    }
+}
