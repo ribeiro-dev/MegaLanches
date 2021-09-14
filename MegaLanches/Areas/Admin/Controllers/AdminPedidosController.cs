3c2999bcf586d3219127385e88fb64613a1d5cf2
rafaelg <romilson.2r@gmail.com>
2021-09-14 16:57:19 -0300

    Showing order details in Admin Area

--- MegaLanches/Areas/Admin/Controllers/AdminPedidosController.cs
+++ MegaLanches/Areas/Admin/Controllers/AdminPedidosController.cs
@@ -1,5 +1,6 @@
 ﻿using MegaLanches.Data;
 using MegaLanches.Models;
+using MegaLanches.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
@@ -19,6 +20,27 @@ namespace MegaLanches.Areas.Admin.Controllers
         public AdminPedidosController(MegaLanchesContext context)
         {
             _context = context;
+        }
+
+        public IActionResult PedidoLanches(int? id)
+        {
+            var pedido = _context.Pedidos.Include(pd => pd.PedidoItens)
+                        .ThenInclude(l => l.Lanche)
+                        .FirstOrDefault(p => p.PedidoId == id);
+
+            if (pedido == null)
+            {
+                Response.StatusCode = 404;
+                return View("PedidoNotFound", id.Value);
+            }
+
+            PedidoLancheViewModel pedidoLanches = new PedidoLancheViewModel()
+            {
+                Pedido = pedido,
+                PedidoDetalhes = pedido.PedidoItens
+            };
+
+            return View(pedidoLanches);
         }
 
         // GET: Admin/AdminPedidos
---
+++ MegaLanches/ViewModels/PedidoLancheViewModel.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using MegaLanches.Models;
+
+namespace MegaLanches.ViewModels
+{
+    public class PedidoLancheViewModel
+    {
+        public Pedido Pedido { get; set; }
+        public IEnumerable<PedidoDetalhe> PedidoDetalhes { get; set; }
+    }
+}
