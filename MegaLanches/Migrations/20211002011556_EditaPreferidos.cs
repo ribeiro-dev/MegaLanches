c21b770306e5e147e22d1d4338ca8cdfdebe1c61
rafaelg <romilson.2r@gmail.com>
2021-10-05 19:53:41 -0300

    Change favorites and edit Home/Index View

---
+++ MegaLanches/Migrations/20211002011556_EditaPreferidos.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MegaLanches.Migrations
+{
+    public partial class EditaPreferidos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE Lanches SET IsPreferido = 1 WHERE CategoriaId = 4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE Lanches SET IsPreferido = 0 WHERE CategoriaId = 4");
+
+        }
+    }
+}
